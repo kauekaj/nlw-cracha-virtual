776265636b72b88162a4bc25f982edbacd0987f9
Kauê Jacyntho <kauekaj@gmail.com>
2021-10-22 18:32:14 -0300

    Using GitHub API

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ const LinksSocialMedia = {
   }
 
   changeSocialMediaLinks()
+
+  function getGitHubProfileInfos () {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+
+  }
+
+  getGitHubProfileInfos()
